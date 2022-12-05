148b047f03e42e1bf343a1b68147c205a0b7746d
ninahigson1 <nina.higson@live.co.uk>
2022-12-05 16:16:48 +0000

    added index.html

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express ();
 
-// //when app recieves a GET request to index page, do this...
-// app.get('/', (req, res) =>{
-//     res.sendFile('hello world!');
-// });
+//when app recieves a GET request to index page, do this...
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(3000, () => {
     console.log('app listening on http://localhost:3000');
