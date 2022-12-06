2d9253b9404bd3d9e7f241aa703888bbad12f2a3
ninahigson1 <nina.higson@live.co.uk>
2022-12-06 14:35:24 +0000

    change handlebars

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const { engine } = require('express-handlebars');
 const app = express ();
 
 
-app.engine('.handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.set('views','./views');
 
 
@@ -16,19 +16,20 @@ app.set('views','./views');
 
 //when app recieves a GET request to index page, do this...
 app.get('/', (req, res) =>{
-    res.render('home');
+    let randNum = Math.floor(Math.random()* 10)
+    res.render('home', {number: randNum, pageName: 'Home'});
 });
 
 app.get('/signup', (req, res) =>{
-    res.render('signup');
+    res.render('signup', {pageName: 'signup'});
 })
 
 app.get('/login', (req, res) =>{
-    res.render('login');
+    res.render('login', {pageName: 'login'});
 })
 
 app.get('/about', (req, res) =>{
-    res.render('about');
+    res.render('about', {pageName: 'about'});
 })
 app.listen(3000, () => {
     console.log('app listening on http://localhost:3000');
