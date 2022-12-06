88bf367815d443d2392ce8e2670459917eeb6885
ninahigson1 <nina.higson@live.co.uk>
2022-12-06 11:59:37 +0000

    implement handlebars

--- index.js
+++ index.js
@@ -6,11 +6,30 @@ const { engine } = require('express-handlebars');
 
 const app = express ();
 
+
+app.engine('.handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views','./views');
+
+
+
+
 //when app recieves a GET request to index page, do this...
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.render('home');
 });
 
+app.get('/signup', (req, res) =>{
+    res.render('signup');
+})
+
+app.get('/login', (req, res) =>{
+    res.render('login');
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about');
+})
 app.listen(3000, () => {
     console.log('app listening on http://localhost:3000');
 });
